de45e5045c56d10493bfd601a7fa5c8baaf4c0fd
m27afk <manushreshta27@gmail.com>
2022-06-08 00:07:06 +0530

    intialised

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Navbar from './components/navbar';
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
---
+++ src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+const Header= styled.div`
+        height:60px;
+        background-color:black;
+        color:white;
+        
+    `
+export default function Navbar()
+{
+    
+    return(
+        <div>
+            <Header>
+                hello
+            </Header>
+        </div>
+    )
+}
