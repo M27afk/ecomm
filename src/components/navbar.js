44086a333caee4e1f2b4688ea6746a3293552751
m27afk <manushreshta27@gmail.com>
2022-06-10 00:16:59 +0530

    added navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,86 @@
+
 import React from 'react'
 import styled from 'styled-components'
-const Header= styled.div`
-        height:60px;
-        background-color:black;
-        color:white;
-        
+
+
+const Container= styled.div`
+        height:60px;        
     `
+const Wrapper=styled.div`
+padding:10px 20px;
+display:flex;
+align-items: center;
+justify-content:space-between;
+text-align:center;
+`
+const Left=styled.div`
+flex:1;
+display: flex;
+align-items: center;
+
+`
+const Language=styled.div`
+font-size:14px;
+cursor:pointer;
+
+`
+
+const Input=styled.input`
+border: none;
+`
+const SearchContainer=styled.div`
+border: 0.5px solid black;
+margin-left:25px;
+padding:5px;
+
+`
+const Center=styled.div`
+flex:2;
+
+`
+const Brand=styled.h1`
+font-weight: 1500;
+`
+
+
+const Right=styled.div`
+flex:1;
+display: flex;
+justify-content:space-evenly;
+`
+const MenuItem=styled.div`
+font-size:14px;
+cursor:pointer;
+
+`
+
+
 export default function Navbar()
 {
     
     return(
         <div>
-            <Header>
-                hello
-            </Header>
+            <Container>
+                <Wrapper>
+                    <Left>
+                        <Language>
+                            EN
+                        </Language>
+                        <SearchContainer>
+                          <Input></Input>
+                          I
+                        </SearchContainer>
+                    </Left>
+                    <Center>
+                        <Brand>zuDio</Brand>
+                    </Center>
+                    <Right>
+                        <MenuItem>REGISTER</MenuItem>
+                        <MenuItem>SIGN IN</MenuItem>
+
+                    </Right>
+                </Wrapper>
+            </Container>
         </div>
     )
 }
